44a07260b9936c7c919f9f33d98a1aa205f9d845
aleenabasil <137114907+aleenabasil@users.noreply.github.com>
2025-03-14 14:07:33 -0700

    Update TransportationPlannerCommandLine.cpp

--- src/TransportationPlannerCommandLine.cpp
+++ src/TransportationPlannerCommandLine.cpp
@@ -1,1 +1,26 @@
+#include "TransportationPlannerCommandLine.h"
+
+struct CTransportationPlannerCommandLine::SImplementation{
+    SImplementation() {
+    }
+
+    ~SImplementation() {
+    }
+
+    bool ProcessCommands() {
+        return true;
+    }
+};
+
+CTransportationPlannerCommandLine::CTransportationPlannerCommandLine(std::shared_ptr<CDataSource> cmdsrc, std::shared_ptr<CDataSink> outsink, std::shared_ptr<CDataSink> errsink, std::shared_ptr<CDataFactory> results, std::shared_ptr<CTransportationPlanner> planner) {
+    DImplementation = std::make_unique<SImplementation>();
+}
+
+CTransportationPlannerCommandLine::~CTransportationPlannerCommandLine() {
+
+}
+
+bool CTransportationPlannerCommandLine::ProcessCommands() {
+    return DImplementation->ProcessCommands();
+}
 

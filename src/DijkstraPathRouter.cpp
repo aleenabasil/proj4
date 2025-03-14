a88305c3279205217e3ebea66965a326f9ae3f4f
aleenabasil <137114907+aleenabasil@users.noreply.github.com>
2025-03-13 17:13:33 -0700

    Update DijkstraPathRouter.cpp

--- src/DijkstraPathRouter.cpp
+++ src/DijkstraPathRouter.cpp
@@ -1,40 +1,103 @@
 #include "DijkstraPathRouter.h"
-#include <memory>
-#include <string>
-#include <unordered_map>
+#include <limits>
+#include <queue>
 #include <vector>
-#include <iostream>
+#include <unordered_map>
+#include <algorithm>
+#include <any>
+#include <memory>
+#include <chrono>
+
+struct CDijkstraPathRouter::SImplementation{
+    
+    struct ThisVertex{
+        TVertexID ID; 
+        std::any Tag; 
+        std::unordered_map<TVertexID,double> edges; 
+        std::vector<TVertexID> path; 
+        
+        ~ThisVertex() = default;
+
+        TVertexID GetVertexID() const noexcept{
+            return ID;
+        }
+        std::any GetTag() const noexcept{
+            return Tag;
+        }
+        std::vector<TVertexID> GetNeighbors() const noexcept{
+            return path;
+        }
+        double GetWeight(TVertexID to) const noexcept{
+            auto it = edges.find(to);
+            if(it != edges.end()){
+                return it->second;
+            }
+            return std::numeric_limits<double>::infinity();
+        }
 
 
+    };
+
+    std::vector<std::shared_ptr<ThisVertex>> vertices; 
+    TVertexID nextID; 
+
+    std::size_t VertexCount() const noexcept{
+        return vertices.size();
+    }
+
+    TVertexID AddVertex(std::any tag) noexcept{
+        auto newVertex = std::make_shared<ThisVertex>();
+        newVertex->ID = nextID;
+        newVertex->Tag = tag;
+        vertices.push_back(newVertex);
+        nextID++;
+        return newVertex->ID;
+    }
+};
 
 // CDijkstraPathRouter member functions
 // Constructor for the Dijkstra Path Router
-CDijkstraPathRouter();
+CDijkstraPathRouter::CDijkstraPathRouter(){
+    DImplementation = std::make_unique<SImplementation>();
+}
 
 // Destructor for the Dijkstra Path Router
-~CDijkstraPathRouter();
+CDijkstraPathRouter::~CDijkstraPathRouter() = default;
 
 // Returns the number of vertices in the path router
-std::size_t VertexCount() const noexcept;
+std::size_t CDijkstraPathRouter::VertexCount() const noexcept{
+    return DImplementation->VertexCount();
+}
 
 // Adds a vertex with the tag provided. The tag can be of any type.
-TVertexID AddVertex(std::any tag) noexcept;
+CPathRouter::TVertexID CDijkstraPathRouter::AddVertex(std::any tag) noexcept{
+
+}
 
 // Gets the tag of the vertex specified by id if id is in the path router.
 // A std::any() is returned if id is not a valid vertex ID.
-std::any GetVertexTag(TVertexID id) const noexcept;
+std::any CDijkstraPathRouter::GetVertexTag(TVertexID id) const noexcept{
+
+}
 
 // Adds an edge between src and dest vertices with a weight of weight. If
 // bidir is set to true an additional edge between dest and src is added. If
 // src or dest nodes do not exist, or if the weight is negative the AddEdge
 // will return false, otherwise it returns true.
-bool AddEdge(TVertexID src, TVertexID dest, double weight, bool bidir =
-false) noexcept;
+bool CDijkstraPathRouter::AddEdge(TVertexID src, TVertexID dest, double weight, bool bidir =
+false) noexcept{
+
+}
 
 // Allows the path router to do any desired precomputation up to the deadline
-bool Precompute(std::chrono::steady_clock::time_point deadline) noexcept;
+bool CDijkstraPathRouter::Precompute(std::chrono::steady_clock::time_point deadline) noexcept{
+
+}
 
 // Returns the path distance of the path from src to dest, and fills out path
 // with vertices. If no path exists NoPathExists is returned.
-double FindShortestPath(TVertexID src, TVertexID dest, std::vector<TVertexID>
-&path) noexcept;
+double CDijkstraPathRouter::FindShortestPath(TVertexID src, TVertexID dest, std::vector<TVertexID>
+&path) noexcept{
+
+}
+

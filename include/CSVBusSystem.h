92b77752a3b6a034331ceb256af64f2fdeed035c
aleenabasil <137114907+aleenabasil@users.noreply.github.com>
2025-03-14 09:29:53 -0700

    Update CSVBusSystem.h

--- include/CSVBusSystem.h
+++ include/CSVBusSystem.h
@@ -7,8 +7,25 @@
 class CCSVBusSystem : public CBusSystem{
     private:
         struct SImplementation;
-        struct SStop;
-        struct SRoute;
+        struct SStop : public CBusSystem::SStop {
+            TStopID StopID;
+            CStreetMap::TNodeID val;
+            TStopID ID() const noexcept override {return StopID;}
+    
+            CStreetMap::TNodeID NodeID() const noexcept override {return val;}
+        };
+    
+        struct SRoute : public CBusSystem::SRoute {
+            std::string RouteName;
+            std::vector<TStopID> RouteStops;
+    
+            std::string Name() const noexcept override {return RouteName;}
+    
+            std::size_t StopCount() const noexcept override {return RouteStops.size();}
+    
+            TStopID GetStopID(std::size_t index) const noexcept override {
+                return (index < RouteStops.size()) ? RouteStops[index] : CBusSystem::InvalidStopID;}
+        };
         std::unique_ptr< SImplementation > DImplementation;
     public:
         CCSVBusSystem(std::shared_ptr< CDSVReader > stopsrc, std::shared_ptr< CDSVReader > routesrc);
@@ -16,10 +33,10 @@ class CCSVBusSystem : public CBusSystem{
 
         std::size_t StopCount() const noexcept override;
         std::size_t RouteCount() const noexcept override;
-        std::shared_ptr<SStop> StopByIndex(std::size_t index) const noexcept override;
-        std::shared_ptr<SStop> StopByID(TStopID id) const noexcept override;
-        std::shared_ptr<SRoute> RouteByIndex(std::size_t index) const noexcept override;
-        std::shared_ptr<SRoute> RouteByName(const std::string &name) const noexcept override;
+        std::shared_ptr<CBusSystem::SStop> StopByIndex(std::size_t index) const noexcept override;
+        std::shared_ptr<CBusSystem::SStop> StopByID(TStopID id) const noexcept override;
+        std::shared_ptr<CBusSystem::SRoute> RouteByIndex(std::size_t index) const noexcept override;
+        std::shared_ptr<CBusSystem::SRoute> RouteByName(const std::string &name) const noexcept override;
 };
 
 #endif

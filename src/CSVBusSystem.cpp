8c00e937a45f626d33ede99ca56aa71e1f33d630
aleenabasil <137114907+aleenabasil@users.noreply.github.com>
2025-03-14 09:34:29 -0700

    Update CSVBusSystem.cpp

--- src/CSVBusSystem.cpp
+++ src/CSVBusSystem.cpp
@@ -7,50 +7,6 @@
 #include "DSVReader.h"    
 #include "XMLReader.h"
 
-// Class for stop structure
-class CCSVBusSystem::SStop : public CBusSystem::SStop {
-    public:
-        TStopID StopID; 
-        CStreetMap::TNodeID val;
-
-        // Bus System Stop member functions
-        // Returns the stop id of the stop
-        TStopID ID() const noexcept override {
-            return StopID;
-        }
-
-        // Returns the node id of the bus stop
-        CStreetMap::TNodeID NodeID() const noexcept override {
-            return val;
-        }
-};
-
-// Class for route structure
-class CCSVBusSystem::SRoute : public CBusSystem::SRoute {
-    public:
-        std::string RouteName;  
-        std::vector<TStopID> RouteStops; 
-        
-        // Bus System Route member functions
-        // Returns the name of the route
-        std::string Name() const noexcept override {
-            return RouteName;
-        }
-
-        // Returns the number of stops on the route
-        std::size_t StopCount() const noexcept override {
-            return RouteStops.size();
-        }
-
-        // Returns the stop id specified by the index, returns InvalidStopID if index
-        // is greater than or equal to StopCount()  
-        TStopID GetStopID(std::size_t index) const noexcept override {
-            if (index >= RouteStops.size()) {
-                return CBusSystem::InvalidStopID;  
-            }
-            return RouteStops[index];
-        }
-};
 
 // Implementation structure for the CSV Bus System
 struct CCSVBusSystem::SImplementation{
